24af681ba838d657f7e9347d05ec323d660ab490
Huydev0808 <phamngohoanghuy95@gmail.com>
2024-02-21 15:42:35 +0700

    Fetch API Json Server

--- README.md
+++ README.md
@@ -3,16 +3,24 @@
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
 
 Developer: Huy Pham
-* Notes about React Hooks:
-  + useState: khi state thay đổi, components re-render lại, cập nhật DOM => hiển thị ra ngoài
-  + useEffect: khi component re-render lại thì useEffect hoạt động (run).
-    Chuẩn khai báo useEffect
-    
-        useEffect(() => {
-        console.log("use effect ran");
-        console.log(blogs);
-        }, []);
 
-    [] mãng rỗng này là 1 dependency: giúp useEffect ko re-render khi component thay đổi state
-    
-  
+-  Notes about React Hooks:
+
+   -  useState: khi state thay đổi, components re-render lại, cập nhật DOM => hiển thị ra ngoài
+   -  useEffect: khi component re-render lại thì useEffect hoạt động (run).
+      Chuẩn khai báo useEffect
+
+          useEffect(() => {
+          console.log("use effect ran");
+          console.log(blogs);
+          }, []);
+
+      [] mãng rỗng này là 1 dependency: giúp useEffect ko re-render khi component thay đổi state
+
+-  useEffect chỉ render lại khi setNewName (vì đã truyền newName vào, nếu muốn render lại cho cả tác vụ handleDelete thì truyền blogs vào)
+-  useEffect sẽ không render lại (chỉ render 1 lần duy nhất) nếu [ ] rỗng
+
+-  /blog ->>> GET ->>> Fetch all blogs
+-  /blogs/{id} ->>> GET ->>> Fetch a single blog
+-  /blogs ->>> POST ->>> Add a new blog
+-  /blogs/{id} ->>> DELETE ->>> Delete a blog
--- src/Components/CpnWriteBlog/Home.jsx
+++ src/Components/CpnWriteBlog/Home.jsx
@@ -1,59 +1,23 @@
 import { useState } from "react";
+import { useEffect } from "react";
 import BlogList from "../BlogList/BlogList";
 const Home = () => {
-    //Ham khong truyen tham so
-    /*const handleClick = () => {
-        console.log("Hello, Join");
-    };*/
-    //Ham co truyen tham so
-    const handleClickAgain = (name) => {
-        console.log('Hello ' + name);
-    }
-    //Su dung useState
-    const [name, setName] = useState("Ngan Le");
-    const handleClickName = () => {
-        setName('Huy Pham');
-    }
-    //Tao blog
-    const [blogs, setBlog] = useState([
-        {
-            title: "Detail 1",
-            body: "Lorem ipsum, dolor sit amet, consectetur adipisicing elit. Libero dolorum amet quam tenetur doloribus itaque quas praesentium. Voluptas, quas dolores.",
-            author: "john",
-            id: 1,
-        },
-        {
-            title: "Detail 2",
-            body: "Lorem ipsum, dolor sit amet, consectetur adipisicing elit. Libero dolorum amet quam tenetur doloribus itaque quas praesentium. Voluptas, quas dolores.",
-            author: "andrew",
-            id: 2,
-        },
-        {
-            title: "Detail 3",
-            body: "Lorem ipsum, dolor sit amet, consectetur adipisicing elit. Libero dolorum amet quam tenetur doloribus itaque quas praesentium. Voluptas, quas dolores.",
-            author: "maria",
-            id: 3,
-        },
-        {
-            title: "Detail 4",
-            body: "Lorem ipsum, dolor sit amet, consectetur adipisicing elit. Libero dolorum amet quam tenetur doloribus itaque quas praesentium. Voluptas, quas dolores.",
-            author: "maria",
-            id: 3,
-        },
-    ]);
+    const [blogs, setBlogs] = useState(null);
+    //Su dung useEffect
+    useEffect(() => {
+        fetch("http://localhost:8000/blogs")
+            .then(res => res.json())
+            .then(data => { setBlogs(data) })
+    }, []);
     const handleDelete = (id) => {
-        const newBlogs = blogs.filter(blog => blog.id != id);
-        setBlog(newBlogs);
-    };
+        const newBlogs = blogs.filter((blog) => blog.id !== id);
+        setBlogs(newBlogs);
+    }
     return (
         <div className="home">
-            <h2>Home Page</h2>
-            <p>Name: {name}</p>
-            <button className="btnClickMe" onClick={handleClickName}>Click me</button>
-            <button className="btnClickMe" onClick={() => { handleClickAgain('Huy Pham') }}>Click me agian</button>
-            <BlogList blogs={blogs} heading="All Details" handleDelete={handleDelete} />
-            <hr className="container mx-auto" />
-            <BlogList blogs={blogs.filter((blog) => blog.author === "maria")} heading="Maria's Details" />
+            {blogs && (<BlogList blogs={blogs}
+                heading="All details"
+                handleDelete={handleDelete} />)}
         </div>
     );
 }
