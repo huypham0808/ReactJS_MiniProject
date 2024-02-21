a280f5405f2d710ded3ee2c97f5e4acfe8a571cd
Huydev0808 <phamngohoanghuy95@gmail.com>
2024-02-21 17:01:49 +0700

    Handle error

--- src/Components/CpnWriteBlog/Home.jsx
+++ src/Components/CpnWriteBlog/Home.jsx
@@ -3,11 +3,26 @@ import { useEffect } from "react";
 import BlogList from "../BlogList/BlogList";
 const Home = () => {
     const [blogs, setBlogs] = useState(null);
+    const [isPending, setIsPending] = useState(true);
+    const [error, setError] = useState(null);
     //Su dung useEffect
     useEffect(() => {
-        fetch("http://localhost:8000/blogs")
-            .then(res => res.json())
-            .then(data => { setBlogs(data) })
+        fetch("http://localhost:8000/blogsa")
+            .then(res => {
+                if (!res.ok) {
+                    throw Error("Could not fetch the data for that resource");
+                }
+                return res.json();
+            })
+            .then(data => {
+                setBlogs(data);
+                setIsPending(false);
+                setError(null);
+            })
+            .catch(err => {
+                setIsPending(false);
+                setError(err.message);
+            })
     }, []);
     const handleDelete = (id) => {
         const newBlogs = blogs.filter((blog) => blog.id !== id);
@@ -15,6 +30,8 @@ const Home = () => {
     }
     return (
         <div className="home">
+            {error && <div>{error}</div>}
+            {isPending && <p>Loading...</p>}
             {blogs && (<BlogList blogs={blogs}
                 heading="All details"
                 handleDelete={handleDelete} />)}
